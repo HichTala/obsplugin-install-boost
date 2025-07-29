cf374ac1e3ee2e5d160d7dfa283edfcc77c599ba
Hicham Tala <98521878+HichTala@users.noreply.github.com>
2025-07-29 16:46:36 +0200

    Update plugin-main.c

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -19,6 +19,9 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <obs-module.h>
 #include <plugin-support.h>
 
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
